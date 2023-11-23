5c197ce69e571a14498f9e36bd8319d19b45ce6d
Khaidir Fahram <khaidir@fahram.com>
2023-11-23 17:48:29 +0700

    saran 1 : Memberikan penjelasan  komentar

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1
 docker build -t item-app:v1 .
 
+# Melihat daftar image di lokal
 docker images
 
+# Mengubah nama image agar sesuai dengan format Docker Hub
 docker tag item-app:v1 fahram/item-app:v1
 
+# Login ke Docker Hub
 echo $PASSWORD_DOCKER_HUB | docker login -u fahram --password-stdin
 
+# Mengunggah image ke Docker Hub
 docker push fahram/item-app:v1
